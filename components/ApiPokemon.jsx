7b3ecb0693b7cf10b99a77fcd4baf261be1969b2
Zaidan07 <zaidanxgaming@gmail.com>
2024-06-25 09:30:15 +0700

    feat: melakukan replay code untuk pemahaman

--- components/ApiPokemon.jsx
+++ components/ApiPokemon.jsx
@@ -1,20 +1,40 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react";
 
-export default function Pokemon(){
+export default function Pokemon() {
+  const [pokemon, setPokemon] = useState([]);
 
-    const getPokemon = async() => {
-        try {
-            const get = await fetch('https://pokeapi.co/api/v2/pokemon/');
-
-            const getData = await get.json();
-            console.log(getData);
-        } catch (error) {
-            console.log(error)
-        }
+  const getPokemon = async () => {
+    try {
+      const get = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const getData = await get.json();
+      console.log();
+      setPokemon(getData.results);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(()=>{getPokemon()});
-    return(
-        <div></div>
-    )
+  // console.log(pokemon);
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+  return (
+    <div>
+      <ul>
+        {pokemon.map((items, pokemon) => {
+          console.log(pokemon);
+          return (
+            <li key={pokemon}>
+              {items.name}
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon + 1}.png`}
+                alt=""
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
