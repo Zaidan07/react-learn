aa9afade58dc86a25e54645efa0f9e2f6190ad49
Zaidan07 <zaidanxgaming@gmail.com>
2024-06-30 15:59:12 +0700

    feat: mengganti judul

--- components/card/CardComponent.jsx
+++ components/card/CardComponent.jsx
@@ -5,8 +5,8 @@ export default function CardComponent ({pokemon, pokeindex}) {
 
     return <div>
         <Card>
-        <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeindex + 1}.png`} alt="" />
         <Text><h1>{pokemon}</h1></Text>
+        <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokeindex + 1}.png`} alt="" />
         </Card>
     </div>
 }
